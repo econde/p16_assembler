d7ae191d84066635ed7c51a980079ac46808e3ba
Ezequiel <ezeq@cc.isel.ipl.pt>
2022-09-26 18:41:22 +0100

    Eliminação da macro ABSOLUTE(x)

--- src/utils.h
+++ src/utils.h
@@ -18,8 +18,8 @@ limitations under the License.
 // Created by ezequiel on 23-03-2018.
 //
 
-#ifndef AS_UTILS_H
-#define AS_UTILS_H
+#ifndef UTILS_H
+#define UTILS_H
 
 /*			|<-width->|<--offset-->|
 	0000000011111111110000000000000
@@ -30,9 +30,6 @@ limitations under the License.
 #include <stdint.h>
 
 #define MAKE_MASK(width, position) ((~(~0U << static_cast<unsigned>(width)) << static_cast<unsigned>(position)))
-
-#define ABSOLUTE(value) (((value) & (1U << (sizeof(value) * CHAR_BIT - 1))) \
-                        ? static_cast<unsigned>(-(value)) : (value))
 
 static inline int get_bits(uint32_t value, unsigned bit_first, unsigned bit_last) {
 	return (value >> bit_first) & MAKE_MASK(bit_last - bit_first + 1, 0);
